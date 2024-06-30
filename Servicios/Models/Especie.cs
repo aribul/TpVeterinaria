256b91362ae20089556a7db4ad4a9a641401e74c
aribul <bulacio.ariela@gmail.com>
2024-06-30 19:16:35 -0300

    Especie DAO y Especie

---
+++ Servicios/Models/Especie.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Servicios.Models
+{
+    internal class Especie
+    {
+
+        public int idEspecie { get; set; }
+        public string nombre { get; set; }
+        public int edadMadurez { get; set; }
+        public double peso { get; set; }
+
+
+        public Especie(int idEspecie, string nombre, int edadMadurez, double peso)
+        {
+            this.idEspecie = idEspecie;
+            this.nombre = nombre;
+            this.edadMadurez = edadMadurez;
+            this.peso = peso;
+
+        }
+
+        public Especie() { }
+    }
+}
---
+++ Servicios/Repositorios/EspecieDAO.cs
@@ -0,0 +1,66 @@
+﻿using Servicios.Models;
+using Servicios.VeterinariaServices;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Servicios.Repositorios
+{
+    internal class EspecieDAO : DAOConexion
+    {
+        public List<Especie> getAll()
+        {
+
+            IDbConnection conexion = this.prepararConexion();
+
+            IDbCommand comando = conexion.CreateCommand();
+
+            //crea query
+            comando.CommandText = "select * from especies";
+
+            //ejecuta la query
+            IDataReader lector = comando.ExecuteReader();
+
+            //Lista de usuarios
+            List<Especie> list = new List<Especie>();
+
+            //Se recorre la lista
+            while (lector.Read())
+            {
+                //crea usuario por cada fila
+                Especie nuevaEspecie = new Especie()
+                {
+                    idEspecie = lector.GetInt32(0),
+                    nombre = lector.GetString(1),
+                    edadMadurez = lector.GetInt32(2),
+                    peso = lector.GetDouble(3)
+
+                };
+
+                //Agrega a la lista
+                list.Add(nuevaEspecie);
+            }
+
+            return list;
+        }
+
+        //Insertar nuevo usuario a la base
+        public void insert(Especie nEspecie)
+        {
+
+            string query = $"insert into animales (idEspecie, nombreEspecie, edadMadurez, pesoPromedio) VALUES ('{nEspecie.idEspecie}', {nEspecie.nombre}, {nEspecie.edadMadurez}, {nEspecie.peso})";
+
+            IDbConnection conexion = this.prepararConexion();
+
+            IDbCommand comando = conexion.CreateCommand();
+
+            comando.CommandText = query;
+        }
+
+
+
+    }
+}
